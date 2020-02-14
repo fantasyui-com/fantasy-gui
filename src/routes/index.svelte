b1d55a3df6a6be66d83a6c73c1a6a2a2be222c63
Captain Fantasy <captain@fantasyui.com>
2020-02-13 19:46:11 -0500

    Introduced Phosphor Zone Cutomization

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,40 +11,51 @@ import htmlLang from 'highlight.js/lib/languages/xml';
 hljs.registerLanguage('css', cssLang);
 hljs.registerLanguage('html', htmlLang);
 
+// PREFERENCE PANE CONFIGURATION
+
 let preferences = [
 
   {
     id: 'darkness',
     type:'range',
     label: 'Darkness Level',
-    base: .1,
+    fraction: .53,
     precision: 1,
   },
+
   {
     id: 'space',
     type:'range',
     label: 'Empty Space',
-    base: .1,
+    fraction: .87,
+  },
+
+  {
+    id: 'phosphor',
+    type:'range',
+    label: 'Phosphor Zones',
+    fraction: .43,
   },
 
   {
     id: 'glow',
     type:'range',
     label: 'Glow Intensity',
-    base: .3,
+    fraction: .75,
     range:[2,8],
     precision:0,
-    value:-1,
   }
 
 ];
+
+// PREFERENCE DATA PROCESSING
 
 $: configuration = preferences.reduce(function(map, obj) {
     map[obj.id] = obj;
     //if(!obj.range) obj.range = [0,100];
     if(!obj.value) obj.value = 0;
     if(!obj.precision) obj.precision = 0;
-    if(obj.range) obj.value = (obj.range[0] + ((obj.range[1]-obj.range[0]) * obj.base)).toFixed(obj.precision);
+    if(obj.range) obj.value = (obj.range[0] + ((obj.range[1]-obj.range[0]) * obj.fraction)).toFixed(obj.precision);
     return map;
 }, {});
 
@@ -56,14 +67,22 @@ function clamp(val, min, max, fix=0){
 
 // CODE GENERATION
 
-$: database = [
+$: generation = [
 
   {
     name: 'background',
     variant: 'primary',
     declaration: {
-      color: chroma('#649093').brighten( configuration.darkness.base ).hex(),
-      backgroundColor: chroma('#1e3233').darken( configuration.darkness.base ).hex(),
+      color: chroma('#649093').brighten( configuration.darkness.fraction ).hex(),
+      backgroundColor: chroma('#1e3233').darken( configuration.darkness.fraction ).hex(),
+    },
+  },
+
+  {
+    name: 'crt',
+    declaration: {
+      background: `linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%), linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06))`,
+      backgroundSize: `100% ${clamp(configuration.phosphor.fraction,2,8)}px, ${clamp(configuration.phosphor.fraction,3,12)}px 100%`,
     },
   },
 
@@ -81,8 +100,8 @@ $: database = [
     variant: 'small',
     requires:'glow',
     declaration: {
-      boxShadow: `inset 0 0 ${clamp(configuration.glow.base,2,12)}px #9ecaed, 0 0 ${clamp(configuration.glow.base,2,12)}px #9ecaed`,
-      borderRadius: `${clamp(configuration.glow.base,0,4)}px`,
+      boxShadow: `inset 0 0 ${clamp(configuration.glow.fraction,2,22)}px #0f161b, 0 0 ${clamp(configuration.glow.fraction,2,12)}px #9ecaed`,
+      borderRadius: `${clamp(configuration.glow.fraction,0,4)}px`,
     },
   },
 
@@ -91,8 +110,8 @@ $: database = [
     variant: 'large',
     requires:'glow',
     declaration: {
-      boxShadow: `inset 0 0 ${clamp(configuration.glow.base,2,24)}px #9ecaed, 0 0 ${clamp(configuration.glow.base,2,24)}px #9ecaed`,
-      borderRadius: `${clamp(configuration.glow.base,0,20)}px`,
+      boxShadow: `inset 0 0 ${clamp(configuration.glow.fraction,2,34)}px #0f161b, 0 0 ${clamp(configuration.glow.fraction,2,24)}px #9ecaed`,
+      borderRadius: `${clamp(configuration.glow.fraction,0,20)}px`,
     },
   },
 
@@ -100,7 +119,7 @@ $: database = [
     name: 'space',
     variant: 'small',
     declaration: {
-      padding: `${clamp(configuration.space.base,0,.5,2)}rem`,
+      padding: `${clamp(configuration.space.fraction,0,.5,2)}rem`,
     },
   },
 
@@ -108,7 +127,7 @@ $: database = [
     name: 'space',
     variant: 'large',
     declaration: {
-      padding: `${clamp(configuration.space.base,1,3,2)}rem`,
+      padding: `${clamp(configuration.space.fraction,1,3,2)}rem`,
     },
   },
 
@@ -119,14 +138,49 @@ $: database = [
 
 ];
 
+let abbreviated = true;
 
-$: generated = database.reduce(function(object, rule) {
-    const id = [rule.name, rule.variant].filter(i=>i).join('-')
-    const name = camelCase(id);
-    object[name] = rule;
-    rule.selector = `.` + id;
+function abbreviate(string){
+  let result = string;
+
+  if(string === 'small'){
+    result = 'sm'
+  }else if(string === 'large'){
+    result = 'lg'
+  }else if(string === 'background'){
+    result = 'bg'
+  }
+
+  return result;
+}
+$: generated = generation.reduce(function(object, rule) {
+
+    let name = null;
+    if(rule.name){
+      if(abbreviated){
+        name = abbreviate(rule.name)
+      }else{
+        name = rule.name;
+      }
+    }
+
+    let variant = null;
+    if(rule.variant){
+      if(abbreviated){
+        variant = abbreviate(rule.variant)
+      }else{
+        variant = rule.variant;
+      }
+    }
+
+
+    const ruleName = [name, variant].filter(i=>i).join('-');
+    const propertyName = camelCase([rule.name, rule.variant].filter(i=>i).join('-'));
+
+    object[propertyName] = rule;
+    rule.selector = `.` + ruleName;
     rule.declarations = [];
-    if(rule.requires) rule.declarations.push(`/* NOTE: requires base class "${rule.requires}" */`);
+    if(rule.requires) rule.declarations.push(`/* NOTE: requires "${rule.requires}" class: class="${rule.requires} ${ruleName}" */`);
     rule.declarations = rule.declarations.concat( Object.keys(rule.declaration).map(property=>`${kebabCase(property)}: ${rule.declaration[property]};`) )
     rule.code = `${rule.selector} {\n${rule.declarations.map(declaration=>'  '+declaration).join('\n')}\n}`;
     rule.preview = css`${rule.declarations.join('')}`;
@@ -141,47 +195,6 @@ $: generated = database.reduce(function(object, rule) {
 
 
 
-
-
-
-
-
-// $: backgroundPrimaryDirectives = `
-//   color: ${chroma('#649093').brighten( configuration.darkness.base ).hex()};
-//   background-color: ${chroma('#1e3233').darken( configuration.darkness.base ).hex()};
-// `.replace(/^\n|\n$/g,'');
-//
-// $: glowSmDirectives = `
-//   border-style: solid;
-//   border-width: 1px;
-//   border-color: #9ecaed;
-//   box-shadow: inset 0 0 ${clamp(configuration.glow.base,2,12)}px #9ecaed, 0 0 ${clamp(configuration.glow.base,2,12)}px #9ecaed;
-//   border-radius: ${clamp(configuration.glow.base,0,4)}px;
-// `.replace(/^\n|\n$/g,'');
-//
-// $: glowLgDirectives = `
-//   border-style: solid;
-//   border-width: 1px;
-//   border-color: #9ecaed;
-//   box-shadow: inset 0 0 ${clamp(configuration.glow.base,2,24)}px #9ecaed, 0 0 ${clamp(configuration.glow.base,2,24)}px #9ecaed;
-//   border-radius: ${clamp(configuration.glow.base,0,20)}px;
-// `.replace(/^\n|\n$/g,'');
-//
-// $: spaceSmDirectives = `
-//   padding: ${clamp(configuration.space.base,0,.5,2)}rem;
-// `.replace(/^\n|\n$/g,'');
-//
-// $: spaceLgDirectives = `
-//   padding: ${clamp(configuration.space.base,1,3,2)}rem;
-// `.replace(/^\n|\n$/g,'');
-
-// // USED IN VISUAL PREVIEW
-// $: generated.backgroundPrimary.preview = css`${backgroundPrimaryDirectives}`;
-// $: generated.glowSmall.preview = css`${glowSmDirectives}`;
-// $: glowLgDirectivesPreview = css`${glowLgDirectives}`;
-// $: generated.spaceSmall.preview = css`${spaceSmDirectives}`;
-// $: spaceLgDirectivesPreview = css`${spaceLgDirectives}`;
-
 // USED IN GENERATED CODE CODE
 function highlightCss(code){
   return hljs.highlight('css', code).value
@@ -189,33 +202,6 @@ function highlightCss(code){
 
 $: cssCode = highlightCss( Object.keys(generated).map(key=>generated[key].code).join('\n\n') );
 
-//
-// $: cssCode = highlightCss(`
-// .background-primary {
-// ${backgroundPrimaryDirectives}
-// }
-//
-// .glow-sm {
-// ${glowSmDirectives}
-// }
-// .glow-lg {
-// ${glowLgDirectives}
-// }
-//
-// .space-sm {
-// ${spaceSmDirectives}
-// }
-// .space-lg {
-// ${spaceLgDirectives}
-// }
-// `);
-//
-
-/*
-TODO: ADD CRT EFFECT
-background: linear-gradient(rgba(18, 16, 16, 0) 50%, rgba(0, 0, 0, 0.25) 50%), linear-gradient(90deg, rgba(255, 0, 0, 0.06), rgba(0, 255, 0, 0.02), rgba(0, 0, 255, 0.06));
-background-size: 100% 2px, 3px 100%;
-*/
 
 </script>
 
@@ -237,7 +223,7 @@ background-size: 100% 2px, 3px 100%;
 
 
       <div>
-      <form class="{generated.glow.preview} {generated.glowSmall.preview} {generated.spaceLarge.preview}">
+      <form class="{generated.crt.preview} {generated.glow.preview} {generated.glowLarge.preview} {generated.spaceLarge.preview}">
 
             <h1>Payment form</h1>
             <p>Required fields are followed by <strong><abbr title="required">*</abbr></strong>.</p>
@@ -271,26 +257,26 @@ background-size: 100% 2px, 3px 100%;
                     <span>Name: </span>
                     <strong><abbr title="required">*</abbr></strong>
                   </label>
-                  <input  class="{generated.backgroundPrimary.preview} {generated.glowSmall.preview}" type="text" id="name" name="username">
+                  <input  class="{generated.backgroundPrimary.preview} {generated.glow.preview} {generated.glowSmall.preview}" type="text" id="name" name="username">
                 </p>
                 <p class="{generated.spaceSmall.preview}">
                   <label for="mail">
                     <span>E-mail: </span>
                     <strong><abbr title="required">*</abbr></strong>
                   </label>
-                  <input  class="{generated.backgroundPrimary.preview} {generated.glowSmall.preview}" type="text" id="mail" name="usermail">
+                  <input  class="{generated.backgroundPrimary.preview} {generated.glow.preview} {generated.glowSmall.preview}" type="text" id="mail" name="usermail">
                 </p>
                 <p class="{generated.spaceSmall.preview}">
                   <label for="pwd">
                     <span>Password: </span>
                     <strong><abbr title="required">*</abbr></strong>
                   </label>
-                  <input  class="{generated.backgroundPrimary.preview} {generated.glowSmall.preview}" type="text" id="pwd" name="password">
+                  <input  class="{generated.backgroundPrimary.preview} {generated.glow.preview} {generated.glowSmall.preview}" type="text" id="pwd" name="password">
                 </p>
             </section>
 
             <section>
-                <p> <button class="{generated.backgroundPrimary.preview} {generated.glowSmall.preview}" type="submit">Validate the payment</button> </p>
+                <p> <button class="{generated.backgroundPrimary.preview} {generated.glow.preview} {generated.glowSmall.preview}" type="submit">Validate the payment</button> </p>
             </section>
         </form>
 
@@ -308,10 +294,10 @@ background-size: 100% 2px, 3px 100%;
 
       {#if preference.type == 'range'}
         <div class="card-text">
-          <label for="id{capitalize(preference.id)}"><small class="text-muted">{preference.label} ({configuration[preference.id].value||preference.base})</small></label>
+          <label for="id{capitalize(preference.id)}"><small class="text-muted">{preference.label} ({configuration[preference.id].value||preference.fraction})</small></label>
           <div class="input-group mb-3">
             <div class="custom-control custom-range">
-              <input type="range" class="w-100" bind:value={preference.base} min="0" max="1" step="0.01" id="id{capitalize(preference.id)}">
+              <input type="range" class="w-100" bind:value={preference.fraction} min="0" max="1" step="0.01" id="id{capitalize(preference.id)}">
             </div>
           </div>
         </div>
@@ -333,15 +319,23 @@ background-size: 100% 2px, 3px 100%;
 
       <div class="col p-0 bg-dark text-white shadow border-top border-secondary" style="min-height: 20rem;">
         <h4 class="mb-0 p-2">Generated Source-code</h4>
-        <div class="mb-3 p-2"><small class="text-muted">copy the code below</small></div>
+
+        <div class="p-2">
+          Abbreviate Names <small class="text-muted">(activate for bootstrap naming convention compatibility &middot; recommended)</small>
+          <input class="d-block" type=checkbox bind:checked={abbreviated}>
+        </div>
+
+        <div class="mb-3 p-2">
+        <small class="text-warning">Tip:</small> <small class="text-muted">Copy the code below into your CSS file.</small>
+        </div>
 
 
-
+<!--
         <pre class="mb-0 shadow">
         <code class="language-css css hljs">
         {JSON.stringify(generated,null,'  ')}
         </code>
-        </pre>
+        </pre> -->
 
 
         <pre class="mb-0 shadow">
